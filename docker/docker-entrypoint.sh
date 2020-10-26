02b3f342217e5060d7a8b3b50d9ed3ab1be07935
Tom Dugan <thomas.dugan@adhocteam.us>
2020-10-26 16:30:32 -0400

    testing

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -4,8 +4,9 @@ set -e
 
 # activate our virtual environment here
 . /opt/pysetup/.venv/bin/activate
+/usr/sbin/sshd
 
 # You can put other setup logic here
-exec /usr/bin/ssm-parent --plain-path "/td/envvars/lab/" run -- env LOG_LOCATION=console "$@"
+#exec /usr/bin/ssm-parent --plain-path "/td/envvars/lab/" run -- env LOG_LOCATION=console "$@"
 # Evaluating passed command:
-#exec "$@"
+exec "$@"
